fd1aadeab57eb2688692739248c17976e3631674
StanovskyDev <stas@vdb.com.pl>
2022-03-31 10:25:34 +0200

    Add move functions Added the functions for generating the computer's move and for reading the players input. They return their values as described in this dictionary: {"rock" : 0, "paper" : 1, "scissors" : 2, inputError : -1}

--- script.js
+++ script.js
@@ -1,3 +1,18 @@
-let computersMove = function(){
+function computersMove(){
+    return Math.floor(Math.random() * 2)
+}
 
+function playersMove(){
+    let input = prompt("Type in your move(rock, paper, scissors):")
+    input.toLowerCase();
+    switch (input){
+        case "rock":
+            return 0
+        case "paper":
+            return 1
+        case "scissors":
+            return 2;
+        default:
+            return -1;
+    }
 }
