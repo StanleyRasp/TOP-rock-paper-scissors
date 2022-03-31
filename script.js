1a01df00de7dd9fbf8e4579b5cb9aa4317e4b4ef
StanovskyDev <stas@vdb.com.pl>
2022-03-31 11:08:26 +0200

    Create the game function and optimalization Created the game function which manages the IO, score, number of rounds and the winning conditions. I also optimised the code with a hash table to a average O(rounds).

--- script.js
+++ script.js
@@ -1,18 +1,65 @@
+const moveIndexes = {
+    "rock" : 2,
+    "paper" : 1,
+    "scissors" : 0
+};
+
+const indexMoves = {
+    2 : "rock",
+    1 : "paper",
+    0 : "scissors"
+};
+
 function computersMove(){
-    return Math.floor(Math.random() * 2)
+    return Math.floor(Math.random() * 2);
 }
 
 function playersMove(){
-    let input = prompt("Type in your move(rock, paper, scissors):")
+    let input = prompt("Type in your move(rock, paper, scissors):");
     input.toLowerCase();
-    switch (input){
-        case "rock":
-            return 0
-        case "paper":
-            return 1
-        case "scissors":
-            return 2;
-        default:
-            return -1;
+    if(moveIndexes[input] != undefined){
+        return moveIndexes[input];
+    }
+    return -1;
+}
+
+function game(rounds){
+    let playerScore = 0, computerScore = 0;
+    const maxScore = Math.floor(rounds/2);
+    for (let i = 0; i < rounds; ++i){
+        
+        alert(
+            `Round ${i+1}/${rounds}\n
+        Score:\n
+        Player: ${playerScore} points\n
+        Computer: ${computerScore} points`
+        )
+
+        let player = playersMove();
+        let computer = computersMove();
+        alert(`Computers move: ${indexMoves[computer]}`);
+
+        if (player === -1){
+            alert("A mistake occured, try again!")
+            i--;
+        } else if ((player + 1) % 3 === computer){
+            alert("You Won!");
+            playerScore ++;
+        } else if ((computer + 1) % 3 === player){
+            alert("I Won!");
+            computerScore ++;
+        } else {
+            alert("It's a tie, let's try again!")
+            i--;
+        }
+
+        if (playerScore > maxScore){
+            alert("Bravo! You won the game!")
+            break;
+        }
+        if (computerScore > maxScore){
+            alert("Ha Ha! I won the game! Let's go again.")
+            break;
+        }
     }
 }
